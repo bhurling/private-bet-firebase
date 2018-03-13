73227c3a8e45b90057db418373d828c90ff29d7a
Bjoern Hurling <bjoern@wunder.org>
2018-03-13 22:35:45 +0100

    show tiny title on add screen

--- app/src/main/java/io/bhurling/privatebet/add/AddBetActivity.kt
+++ app/src/main/java/io/bhurling/privatebet/add/AddBetActivity.kt
@@ -1,5 +1,20 @@
 package io.bhurling.privatebet.add
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import io.bhurling.privatebet.R
+import kotterknife.bindView
 
-class AddBetActivity : AppCompatActivity()
+class AddBetActivity : AppCompatActivity() {
+
+    private val toolbar: Toolbar by bindView(R.id.bets_add_toolbar)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
