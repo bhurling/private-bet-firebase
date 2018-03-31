77497566fcca77588523796d81affb58cbba96c7
Bjoern Hurling <bjoern@wunder.org>
2018-03-31 10:38:34 +0200

    show toolbar and back button

--- app/src/main/java/io/bhurling/privatebet/friends/InviteActivity.kt
+++ app/src/main/java/io/bhurling/privatebet/friends/InviteActivity.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import io.bhurling.privatebet.R
 import io.reactivex.Observable
 import kotterknife.bindView
@@ -14,17 +16,31 @@ internal class InviteActivity: AppCompatActivity(), InvitePresenter.View {
 
     private val presenter: InvitePresenter by inject()
 
+    private val toolbar: Toolbar by bindView(R.id.toolbar)
     private val list: RecyclerView by bindView(R.id.invite_list)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_invite)
 
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         list.layoutManager = LinearLayoutManager(this)
         list.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
         list.adapter = InviteAdapter()
 
         presenter.attachView(this)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroy() {
