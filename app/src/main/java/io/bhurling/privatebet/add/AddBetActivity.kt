520da57bf39ba50e07091e68c051120f8691fbd8
Bjoern Hurling <bjoern@wunder.org>
2018-04-01 16:35:35 +0200

    apply colors from design

--- app/src/main/java/io/bhurling/privatebet/add/AddBetActivity.kt
+++ app/src/main/java/io/bhurling/privatebet/add/AddBetActivity.kt
@@ -3,12 +3,15 @@ package io.bhurling.privatebet.add
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.widget.EditText
 import io.bhurling.privatebet.R
 import kotterknife.bindView
 
 class AddBetActivity : AppCompatActivity() {
 
     private val toolbar: Toolbar by bindView(R.id.bets_add_toolbar)
+    private val statement: EditText by bindView(R.id.bets_add_statement)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +19,18 @@ class AddBetActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        statement.setHorizontallyScrolling(false)
+        statement.maxLines = 100
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
