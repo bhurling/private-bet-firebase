8608e895003e359c376c93b846da7936e4c62e41
Bjoern Hurling <bjoern@wunder.org>
2018-04-16 22:57:41 +0200

    have non null view in presenters

--- app/src/main/java/io/bhurling/privatebet/arch/Presenter.kt
+++ app/src/main/java/io/bhurling/privatebet/arch/Presenter.kt
@@ -8,18 +8,24 @@ abstract class Presenter<V : Presenter.View> {
 
     protected val disposables = CompositeDisposable()
 
-    protected var view: V? = null
+    private var _view: V? = null
+
+    protected val view: V
+        get() {
+            _view?.let { return it }
+                    ?: error("Cannot access view before attachView() or after detachView()")
+        }
 
     @CallSuper
     open fun attachView(view: V) {
-        this.view = view
+        this._view = view
     }
 
     @CallSuper
     fun detachView() {
         this.disposables.dispose()
 
-        this.view = null
+        this._view = null
     }
 
     interface View
