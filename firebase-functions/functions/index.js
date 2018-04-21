1ac052df12f0781e2e070064103dde2637cb0520
Bjoern Hurling <bjoern@wunder.org>
2018-04-21 12:26:50 +0200

    do not send notification if link was just confirmed

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -4,52 +4,65 @@ admin.initializeApp();
 
 exports.sendNotificationToInvitee = functions.database.ref('/links/{receiverUid}/incoming/{senderUid}')
     .onWrite((change, context) => {
-        console.log(change)
-        console.log(context)
-
         const receiverUid = context.params.receiverUid;
         const senderUid = context.params.senderUid;
 
+        console.log(`Incoming change for /links/${receiverUid}/incoming/${senderUid}`);
+
         if (!change.after.val()) {
-            console.log('Invitation for', receiverUid, 'from', senderUid, 'has been removed.');
+            console.log('Invitation for', receiverUid, 'from', senderUid, 'has been removed');
 
             return 0;
         }
 
-        console.log(senderUid, 'wants to invite', receiverUid, '.');
+        console.log(`${senderUid} wants to invite ${receiverUid}`);
+        console.log(`Checking other link /links/${senderUid}/incoming/${receiverUid}`)
 
-        const getDeviceTokensPromise = admin.database()
-                .ref(`/devices/${receiverUid}`).once('value');
+        const otherLinkPromise = admin.database()
+                .ref(`/links/${senderUid}/incoming/${receiverUid}`).once('value');
 
-        const getUserProfilePromise = admin.auth().getUser(senderUid);
+        return Promise.all([otherLinkPromise]).then(results => {
+            const linkSnapshot = results[0]
 
-        return Promise.all([getDeviceTokensPromise, getUserProfilePromise]).then(results => {
-            const tokensSnapshot = results[0]
-            const profileSnapshot = results[1]
-
-            if (tokensSnapshot.numChildren() == 0) {
-                console.log('There are no notification tokens to send to.');
+            if (linkSnapshot.val() == true) {
+                console.log('Other link exists, skip notification');
 
                 return 0;
             }
 
-            console.log('There are', tokensSnapshot.numChildren(), 'tokens to send notifications to.');
-            console.log('Sender profile is', profileSnapshot)
+            const getDeviceTokensPromise = admin.database()
+                    .ref(`/devices/${receiverUid}`).once('value');
 
-            const payload = {
-                notification: {
-                    title: 'You have a new invitation',
-                    body: `${profileSnapshot.displayName} wants to challenge you on Private Bet.`,
-                    icon: profileSnapshot.photoURL
+            const getUserProfilePromise = admin.auth().getUser(senderUid);
+
+            return Promise.all([getDeviceTokensPromise, getUserProfilePromise]).then(results => {
+                const tokensSnapshot = results[0]
+                const profileSnapshot = results[1]
+
+                if (tokensSnapshot.numChildren() == 0) {
+                    console.log('There are no notification tokens to send to');
+
+                    return 0;
                 }
-            };
 
-            const tokens = Object.keys(tokensSnapshot.val());
+                console.log('There are', tokensSnapshot.numChildren(), 'tokens to send notifications to');
+                console.log('Sender profile is', profileSnapshot)
 
-            return admin.messaging().sendToDevice(tokens, payload);
-        }).then((response) => {
-            console.log(response)
+                const payload = {
+                    notification: {
+                        title: 'You have a new invitation',
+                        body: `${profileSnapshot.displayName} wants to challenge you on Private Bet.`,
+                        icon: profileSnapshot.photoURL
+                    }
+                };
 
-            return 0;
-        });
+                const tokens = Object.keys(tokensSnapshot.val());
+
+                return admin.messaging().sendToDevice(tokens, payload).then((response) => {
+                    console.log(response)
+
+                    return 0;
+                });
+            })
+        })
 });
