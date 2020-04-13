7ec9888f31af74525e5c0e2071ad8593dde1d269
Bjoern Hurling <bjoern@wunder.org>
2020-04-13 14:12:46 +0200

    Refactored cloud function for incoming invitations

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -52,7 +52,7 @@ exports.onInvitationDeclined = functions.firestore.document('/links/{receiverUid
         const receiverUid = context.params.receiverUid
         const senderUid = context.params.senderUid
 
-        console.log(`${receiverUid} wants to declined invitation from ${senderUid}`)
+        console.log(`${receiverUid} wants to decline invitation from ${senderUid}`)
 
         const senderLink = await admin.firestore().doc(`/links/${senderUid}/outgoing/${receiverUid}`).get()
 
@@ -61,7 +61,7 @@ exports.onInvitationDeclined = functions.firestore.document('/links/{receiverUid
 
             return admin.firestore().doc(`/links/${senderUid}/outgoing/${receiverUid}`).delete()
         } else {
-            console.log(`Outgoing link fron ${senderUid} did not exist`)
+            console.log(`Outgoing link from ${senderUid} did not exist`)
 
             return null
         }
@@ -87,68 +87,41 @@ exports.onInvitationConfirmed = functions.firestore.document('/links/{receiverUi
         }
 })
 
-exports.sendNotificationToInvitee = functions.firestore.document('/links/{receiverUid}/incoming/{senderUid}')
-    .onWrite((change, context) => {
-        const receiverUid = context.params.receiverUid;
-        const senderUid = context.params.senderUid;
+exports.onInvitationReceived = functions.firestore.document('/links/{receiverUid}/incoming/{senderUid}')
+    .onCreate(async (snapshot, context) => {
+        const receiverUid = context.params.receiverUid
+        const senderUid = context.params.senderUid
 
-        console.log(`Incoming change for /links/${receiverUid}/incoming/${senderUid}`);
+        console.log(`Incoming invitation for ${receiverUid} from ${senderUid}`)
 
-        if (!change.after.data()) {
-            console.log('Invitation for', receiverUid, 'from', senderUid, 'has been removed');
+        const getDeviceTokensPromise = database.fetchOnce(`/devices/${receiverUid}`)
+        const getUserProfilePromise = authentication.getUser(senderUid)
 
-            return 0;
+        const results = await Promise.all([getDeviceTokensPromise, getUserProfilePromise])
+
+        const tokens = Object.keys(results[0].data())
+        const profile = results[1]
+
+        if (tokens.length == 0) {
+            console.log('There are no notification tokens to send to')
+
+            return null
         }
 
-        console.log(`${senderUid} wants to invite ${receiverUid}`);
-        console.log(`Checking other link /links/${senderUid}/incoming/${receiverUid}`)
+        console.log(`There are ${tokens.length} tokens to send notifications to`)
+        console.log(`Sender profile is ${profile}`)
 
-        const otherLinkPromise = database.fetchOnce(`/links/${senderUid}/incoming/${receiverUid}`)
-
-        return Promise.all([otherLinkPromise]).then(results => {
-            const linkSnapshot = results[0]
-
-            if (linkSnapshot.exists) {
-                console.log('Other link exists, skip notification');
-
-                return 0;
+        const payload = {
+            data: {
+                key: 'InvitationNew',
+                id: profile.uid,
+                custom: JSON.stringify({
+                    senderImage: profile.photoURL,
+                    senderId: profile.uid,
+                    senderDisplayName: profile.displayName
+                })
             }
+        }
 
-            const getDeviceTokensPromise = database.fetchOnce(`/devices/${receiverUid}`)
-            const getUserProfilePromise = authentication.getUser(senderUid);
-
-            return Promise.all([getDeviceTokensPromise, getUserProfilePromise]).then(results => {
-                const tokens = Object.keys(results[0].data());
-                const profile = results[1];
-
-                if (tokens.length == 0) {
-                    console.log('There are no notification tokens to send to');
-
-                    return 0;
-                }
-
-                console.log('There are', tokens.length, 'tokens to send notifications to');
-                console.log('Sender profile is', profile)
-
-                const payload = {
-                    data: {
-                        key: 'InvitationNew',
-                        id: profile.uid,
-                        custom: JSON.stringify({
-                            senderImage: profile.photoURL,
-                            senderId: profile.uid,
-                            senderDisplayName: profile.displayName
-                        })
-                    }
-                };
-
-                const messagingPromise = messaging.sendToDevice(tokens, payload)
-
-                return Promise.all([messagingPromise]).then((response) => {
-                    console.log(response)
-
-                    return 0;
-                });
-            })
-        })
-});
+        return messaging.sendToDevice(tokens, payload)
+})
