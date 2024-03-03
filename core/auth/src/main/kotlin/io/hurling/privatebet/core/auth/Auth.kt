62bd93fba9326bbb9ea9b2445bc4ea66edacfea5
Bjoern Hurling <242983+bhurling@users.noreply.github.com>
2024-03-03 12:27:16 +0100

    auth state flow

--- core/auth/src/main/kotlin/io/hurling/privatebet/core/auth/Auth.kt
+++ core/auth/src/main/kotlin/io/hurling/privatebet/core/auth/Auth.kt
@@ -1,11 +1,14 @@
 package io.hurling.privatebet.core.auth
 
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
 interface Auth {
     val currentUserId: String?
+
+    val authState: Flow<AuthState>
 }
 
 @Singleton
@@ -13,5 +16,7 @@ class DefaultAuth @Inject constructor(
     private val firebaseAuth: FirebaseAuth
 ) : Auth {
     override val currentUserId: String?
-        get() = firebaseAuth.currentUser?.uid
+        get() = firebaseAuth.uid
+    override val authState: Flow<AuthState>
+        get() = firebaseAuth.authState
 }
---
+++ core/auth/src/main/kotlin/io/hurling/privatebet/core/auth/AuthState.kt
@@ -0,0 +1,29 @@
+package io.hurling.privatebet.core.auth
+
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.launch
+
+internal val FirebaseAuth.authState: Flow<AuthState>
+    get() = callbackFlow {
+        val listener = FirebaseAuth.AuthStateListener { auth ->
+            val state = auth.uid?.let { uid ->
+                AuthState.Authenticated(uid)
+            } ?: AuthState.NotAuthenticated
+
+            launch { send(state) }
+        }
+
+        addAuthStateListener(listener)
+
+        awaitClose {
+            removeAuthStateListener(listener)
+        }
+    }
+
+sealed interface AuthState {
+    data object NotAuthenticated : AuthState
+    data class Authenticated(val uid: String) : AuthState
+}
